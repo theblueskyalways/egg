240e5be9532620fa7f8379df6849e456de4f0128
Taining Chen <taining.chen@9-a-m.com>
2019-11-21 14:14:38 +0800

    this commit change hello

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = 'hi,欢迎访问 taining.chen 的个人网站。该网站正在紧张建设中。该主人可能去火星探险还未归来，所以在这段时间可以去访问一下www.bilibili.com';
   }
 }
 
--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -14,7 +14,7 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, egg')
+      .expect('hi,欢迎访问 taining.chen 的个人网站。该网站正在紧张建设中。该主人可能去火星探险还未归来，所以在这段时间可以去访问一下www.bilibili.com')
       .expect(200);
   });
 });
