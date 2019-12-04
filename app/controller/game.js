2d59fb0348e6ab63e40c79198c98c06f738d3ede
龙 <775325124@qq.com>
2019-12-04 17:32:14 +0800

    把游戏并到一起了，另一个跳转页删除了，解决冲突。

--- app/controller/game.js
+++ app/controller/game.js
@@ -1,32 +1,22 @@
 const Controller = require('egg').Controller;
 
-class GameController extends Controller{
-    async game() {
-        const { ctx } = this;
-        let title = "我是游戏页"; //向模板传入数据
-        const dataList = {
-            list: [
-              { id: 1, title: '拼图游戏', url: '/game/game_1' },
-              { id: 2, title: '华容道', url: '/game/game_2' }
-            ]
-          };
-        await ctx.render('games/game_list',dataList);
-        // ctx.body = 'hi,欢迎访问 long 的游戏页。该网站正在紧张建设中。该主人可能找陈死狗了，所以在这段时间可以去看一会爱情动作片';
-    }
+class GameController extends Controller {
+    //拼图游戏
     async game_1() {
         const { ctx } = this;
         let title = "我是拼图游戏"; //向模板传入数据
-       
-        await ctx.render('games/game_hua_1',{title:title});
-      
+
+        await ctx.render('games/game_hua_1', { title: title });
+
     }
+    //华容道简版
     async game_2() {
         const { ctx } = this;
         let title = "我是华容道"; //向模板传入数据
-       
-        await ctx.render('games/game_hua_2',{title:title});
-    
+
+        await ctx.render('games/game_hua_2', { title: title });
+
     }
-   
+
 }
 module.exports = GameController
--- app/router.js
+++ app/router.js
@@ -6,9 +6,8 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/game',controller.game.game)
-  router.get('/game/game_1',controller.game.game_1)
-  router.get('/game/game_2',controller.game.game_2)
+  router.get('/game_1',controller.game.game_1)
+  router.get('/game_2',controller.game.game_2)
   router.get('/hero', controller.home.hero);
   router.get('/snake', controller.home.snake);
 };
