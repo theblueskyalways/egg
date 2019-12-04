100d1cf415ebbafc5b0dd2392a7b65783753c2ff
龙 <775325124@qq.com>
2019-12-04 11:44:38 +0800

    新的游戏

--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/game',controller.game.game)
+  router.get('/game/game_1',controller.game.game_1)
+  router.get('/game/game_2',controller.game.game_2)
 };
