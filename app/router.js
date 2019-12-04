563f731590129d9d14fdbe0ef37e9b4b1cc87023
龙 <775325124@qq.com>
2019-12-04 11:53:09 +0800

    解决冲突

--- app/router.js
+++ app/router.js
@@ -6,12 +6,9 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-<<<<<<< HEAD
   router.get('/game',controller.game.game)
   router.get('/game/game_1',controller.game.game_1)
   router.get('/game/game_2',controller.game.game_2)
-=======
   router.get('/hero', controller.home.hero);
   router.get('/snake', controller.home.snake);
->>>>>>> 9df6a844f8a8e13f7bc1ac1304e11ddd769f9735
 };
