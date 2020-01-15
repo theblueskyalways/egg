a08f46ea8506b88ed066a1a83367906395fd6b8c
775325124 <775325124@qq.com>
2020-01-15 16:17:35 +0800

    no message

--- app/controller/user.js
+++ app/controller/user.js
@@ -9,7 +9,7 @@ class HomeController extends Controller {
     const { ctx } = this;
     const { phone, password } = ctx.request.body;
     if (!phone || !password) return this.fail('账户名或密码错误');
-    const result = await ctx.app.mysql.select('user', { where: { phone, password } },'wish');
+    const result = await ctx.app.mysql.select('user', { where: { phone, password,id} });
     if (result.length === 1) return this.success(result);
     this.fail();
   }
